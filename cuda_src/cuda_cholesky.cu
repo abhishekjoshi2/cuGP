c7328fbd177a209ce4856047379e6478abe10c80
Abhishek Joshi <abhishekbjoshi2@gmail.com>
2016-04-15 21:41:03 -0400

    add CUDA setup method

--- cuda_src/cuda_cholesky.cu
+++ cuda_src/cuda_cholesky.cu
@@ -10,8 +10,120 @@ hardcoded_cholesky_2x2()
 	printf("In kernel\n");
 }
 
+void setup()
+{
+    int deviceCount = 0;
+    bool isFastGPU = false;
+    std::string name;
+    cudaError_t err = cudaGetDeviceCount(&deviceCount);
+
+    printf("---------------------------------------------------------\n");
+    printf("Initializing CUDA for cuGP\n");
+    printf("Found %d CUDA devices\n", deviceCount);
+
+    for (int i=0; i<deviceCount; i++) {
+        cudaDeviceProp deviceProps;
+        cudaGetDeviceProperties(&deviceProps, i);
+        name = deviceProps.name;
+        if (name.compare("GeForce GTX 480") == 0
+            || name.compare("GeForce GTX 670") == 0
+            || name.compare("GeForce GTX 780") == 0)
+        {
+            isFastGPU = true;
+        }
+
+        printf("Device %d: %s\n", i, deviceProps.name);
+        printf("   SMs:        %d\n", deviceProps.multiProcessorCount);
+        printf("   Global mem: %.0f MB\n", static_cast<float>(deviceProps.totalGlobalMem) / (1024 * 1024));
+        printf("   CUDA Cap:   %d.%d\n", deviceProps.major, deviceProps.minor);
+    }
+    printf("---------------------------------------------------------\n");
+    if (!isFastGPU)
+    {
+        printf("WARNING: "
+               "You're not running on a fast GPU, please consider using "
+               "NVIDIA GTX 480, 670 or 780.\n");
+        printf("---------------------------------------------------------\n");
+    }
+    
+    // By this time the scene should be loaded.  Now copy all the key
+    // data structures into device memory so they are accessible to
+    // CUDA kernels
+    //
+    // See the CUDA Programmer's Guide for descriptions of
+    // cudaMalloc and cudaMemcpy
+
+    /* cudaMalloc(&cudaDevicePosition, sizeof(float) * 3 * numCircles);
+    cudaMalloc(&cudaDeviceVelocity, sizeof(float) * 3 * numCircles);
+    cudaMalloc(&cudaDeviceColor, sizeof(float) * 3 * numCircles);
+    cudaMalloc(&cudaDeviceRadius, sizeof(float) * numCircles);
+    cudaMalloc(&cudaDeviceImageData, sizeof(float) * 4 * image->width * image->height);
+
+    cudaMemcpy(cudaDevicePosition, position, sizeof(float) * 3 * numCircles, cudaMemcpyHostToDevice);
+    cudaMemcpy(cudaDeviceVelocity, velocity, sizeof(float) * 3 * numCircles, cudaMemcpyHostToDevice);
+    cudaMemcpy(cudaDeviceColor, color, sizeof(float) * 3 * numCircles, cudaMemcpyHostToDevice);
+    cudaMemcpy(cudaDeviceRadius, radius, sizeof(float) * numCircles, cudaMemcpyHostToDevice);
+
+    // Initialize parameters in constant memory.  We didn't talk about
+    // constant memory in class, but the use of read-only constant
+    // memory here is an optimization over just sticking these values
+    // in device global memory.  NVIDIA GPUs have a few special tricks
+    // for optimizing access to constant memory.  Using global memory
+    // here would have worked just as well.  See the Programmer's
+    // Guide for more information about constant memory.
+
+    GlobalConstants params;
+    params.sceneName = sceneName;
+    params.numCircles = numCircles;
+    params.imageWidth = image->width;
+    params.imageHeight = image->height;
+    params.position = cudaDevicePosition;
+    params.velocity = cudaDeviceVelocity;
+    params.color = cudaDeviceColor;
+    params.radius = cudaDeviceRadius;
+    params.imageData = cudaDeviceImageData;
+
+    cudaMemcpyToSymbol(cuConstRendererParams, &params, sizeof(GlobalConstants));
+
+    // also need to copy over the noise lookup tables, so we can
+    // implement noise on the GPU
+    int* permX;
+    int* permY;
+    float* value1D;
+    getNoiseTables(&permX, &permY, &value1D);
+    cudaMemcpyToSymbol(cuConstNoiseXPermutationTable, permX, sizeof(int) * 256);
+    cudaMemcpyToSymbol(cuConstNoiseYPermutationTable, permY, sizeof(int) * 256);
+    cudaMemcpyToSymbol(cuConstNoise1DValueTable, value1D, sizeof(float) * 256);
+
+    // last, copy over the color table that's used by the shading
+    // function for circles in the snowflake demo
+
+    float lookupTable[COLOR_MAP_SIZE][3] = {
+        {1.f, 1.f, 1.f},
+        {1.f, 1.f, 1.f},
+        {.8f, .9f, 1.f},
+        {.8f, .9f, 1.f},
+        {.8f, 0.8f, 1.f},
+    };
+
+    cudaMemcpyToSymbol(cuConstColorRamp, lookupTable, sizeof(float) * 3 * COLOR_MAP_SIZE);
+
+    if(cudaMalloc(&table, sizeof(int) * numCircles * image->width * image->height / 16 / 16) != cudaSuccess )
+    	printf("The giant malloc failed.\n");
+
+    if (cudaMalloc(&counters, sizeof(int) * image->height * image->width / 16 / 16) != cudaSuccess)
+    	printf("The counter malloc fialed.\n");
+
+    if (cudaMalloc(&flagarray, sizeof(int) * numCircles) != cudaSuccess)
+    	printf("The flag array malloc failed.\n");
+
+    cudaMemset((void *)flagarray, 0, sizeof(int) * numCircles); */
+}
+
+
 void run_kernel()
 {
+	setup();
 	printf("Call kernel\n");
 	hardcoded_cholesky_2x2<<<1,1>>>();
 	cudaThreadSynchronize();
