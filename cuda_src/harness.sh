13c915aee16aaaf3a37c9284244c0eb0f4e751ef
Abhishek Joshi <abhishekbjoshi2@gmail.com>
2016-05-02 20:29:52 -0400

    mulinode socket communication working

--- cuda_src/harness.sh
+++ cuda_src/harness.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
+JOBNAME=$PBS_JOBID
+
 cd $PBS_O_WORKDIR
 PATH=$PATH:$PBS_O_PATH
+
+USERNAME=`whoami`
+MASTER=`head -n1 $PBS_NODEFILE`
+NODES=`sort -u $PBS_NODEFILE`
+
 . ~/.bashrc
 . ~/.bash_profile
+
+echo $PBS_NODEFILE > nodes
+
+echo "Username is " $USERNAME
+echo "Master is " $MASTER
+echo "List of nodes is " $NODES
+echo "Also check out the nodes file"
+
+echo "Spawning process on $HOSTNAME ($PBS_NODENUM of $PBS_NUM_NODES)"
+
 HOST=`hostname`
 
 echo "Hostname is " $HOST
@@ -14,4 +31,5 @@ module load gcc-4.9.2
 # /usr/lib64/openmpi/bin/mpirun -np 4 -x LD_LIBRARY_PATH=/usr/local/cuda/lib64/:/usr/local/cuda/lib:/usr/lib64/openmpi/lib:/usr/local/cuda/lib64:/home/15-418/opencv/build/lib:/home/15-418/glog-0.3.3/build/lib:/home/15-418/cudnn-6.5-linux-R1:/home/15-418/caffe/build/lib:/home/15-418/boost_1_57_0/stage/lib:/opt/opt-openmpi/1.8.5rc1/lib:/opt/gcc/4.9.2/lib64:/opt/gcc/4.9.2/lib:/opt/python/lib --hostfile hf -mca btl tcp,self -mca plm_rsh_agent ssh ./multinode_mpi_sample
 #fi
 # cuda-memcheck ./gp
-cuda-memcheck ./gp
+#cuda-memcheck ./gp
+./gp $HOSTNAME > $HOSTNAME.log
